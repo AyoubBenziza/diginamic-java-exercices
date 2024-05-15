9370e076cd17b44da7b0f3cefcd3d117e8a2f97f
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-05-15 14:12:12 +0200

    update: TestCalendar

--- src/Dates/TestCalendar.java
+++ src/Dates/TestCalendar.java
@@ -1,6 +1,8 @@
 package Dates;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
@@ -8,21 +10,23 @@ public class TestCalendar {
     public static void main(String[] args) {
         System.out.println("------ Test Calendar ------");
         Calendar date1 = new GregorianCalendar(2016, Calendar.MAY, 19, 23, 59, 30);
-        System.out.println("Format DD/MM/YYYY : " + date1.get(Calendar.DAY_OF_MONTH) + "/" + (date1.get(Calendar.MONTH) + 1) + "/" + date1.get(Calendar.YEAR));
+        System.out.println("Format DD/MM/YYYY 19/05/2016 : " + new SimpleDateFormat("dd/MM/yyyy").format(date1.getTime()));
 
         Calendar today = new GregorianCalendar();
-        System.out.println("Format DD/MM/YYYY hh:mm:ss : " + today.get(Calendar.DAY_OF_MONTH) + "/" + (today.get(Calendar.MONTH) + 1) + "/" + today.get(Calendar.YEAR) + " " + today.get(Calendar.HOUR_OF_DAY) + ":" + today.get(Calendar.MINUTE) + ":" + today.get(Calendar.SECOND));
+        System.out.println("Format DD/MM/YYYY hh:mm:ss Today : " + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(today.getTime()));
 
         System.out.println("--- Languages ---");
+        new SimpleDateFormat("dd/MM/yyyy").format(new Date());
         // The day and month of today in chinese
-        System.out.println("Today in chinese: " + today.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, new Locale("zh", "CN")) + " " + today.get(Calendar.DAY_OF_MONTH) + " " + today.getDisplayName(Calendar.MONTH, Calendar.LONG, new Locale("zh", "CN")));
+        System.out.println("Today in chinese: " + new SimpleDateFormat("EEEE dd MMMM", Locale.CHINESE).format(today.getTime()));
         // In french
-        System.out.println("Today in french: " + today.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, new Locale("fr", "FR")) + " " + today.get(Calendar.DAY_OF_MONTH) + " " + today.getDisplayName(Calendar.MONTH, Calendar.LONG, new Locale("fr", "FR")));
+        System.out.println("Today in french: " + new SimpleDateFormat("EEEE dd MMMM", Locale.FRENCH).format(today.getTime()));
         // In german
-        System.out.println("Today in german: " + today.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, new Locale("de", "DE")) + " " + today.get(Calendar.DAY_OF_MONTH) + " " + today.getDisplayName(Calendar.MONTH, Calendar.LONG, new Locale("de", "DE")));
+        System.out.println("Today in german: " + new SimpleDateFormat("EEEE dd MMMM", Locale.GERMAN).format(today.getTime()));
         // In russian
-        System.out.println("Today in russian: " + today.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, new Locale("ru", "RU")) + " " + today.get(Calendar.DAY_OF_MONTH) + " " + today.getDisplayName(Calendar.MONTH, Calendar.LONG, new Locale("ru", "RU")));
+        System.out.println("Today in russian: " + new SimpleDateFormat("EEEE dd MMMM", new Locale("ru", "RU")).format(today.getTime()));
         System.out.println("--- Languages ---");
+
         System.out.println("------ Test Calendar ------");
     }
 }
